b52a8f33a33012bb7b4470b9312dd2309fc0edba
Pedro Gutierres <pedro@imperiumsolucoes.com.br>
2023-10-30 14:07:07 -0300

    Melhorias em textos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
 
   constructor(private meta: Meta, private title: Title) {
     this.meta.addTags([
-      { name: 'description', content: 'Encontre a melhor combinação de itens para melhorar a proteção na sua hunt no Tibia, utilizando a calculadora de danos.' },
+      { name: 'description', content: 'Encontre a melhor combinação de itens para melhorar a proteção na sua hunt no Tibia, utilizando a calculadora de danos recebidos.' },
       { name: 'keywords', content: 'Tibia, Calculadora de Danos, Damage Input' },
     ])
     this.title.setTitle("Portal Ze Tibinha");
